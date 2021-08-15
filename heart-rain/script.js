138dd60a3cda5fb9444c5f5abd8cbf2e2d7e6de1
Bazil Farooq <59576110+Bazil21@users.noreply.github.com>
2021-08-15 21:39:33 +0500

    ☑️ompleted

---
+++ heart-rain/script.js
@@ -0,0 +1,22 @@
+
+function createHeart(){
+   const heart =  document.createElement("div");
+
+   heart.classList.add("heart");
+
+
+    heart.innerHTML = "❤️";
+
+    heart.style.left = Math.random() *100 + "vw";
+
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+   document.body.appendChild(heart);
+
+   setTimeout( ()=>{
+       heart.remove();
+   } ,5000);
+
+}
+
+setInterval(createHeart, 300)
